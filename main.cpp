30314966184c2d02fc128154fb2ec9596049f1ac
danielspinard <d.spinardi666@gmail.com>
2022-01-30 17:02:04 -0300

    Make void function to separate terminal output

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+void separator()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "-";
+    }
+
+    std::cout << std::endl;
+}
+
 int main()
 {
     return 1;
