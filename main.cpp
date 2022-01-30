61c089bf0480bc37934ca6923e403fe6145c347e
danielspinard <d.spinardi666@gmail.com>
2022-01-30 17:03:13 -0300

    Instantiating entity and call this methods

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include "entity/employee.cpp"
+#include "entity/developerEmployee.cpp"
+#include "entity/sellerEmployee.cpp"
+
+using std::string;
+using namespace entity;
 
 void separator()
 {
@@ -12,5 +18,22 @@ void separator()
 
 int main()
 {
+    // developer entity
+    developerEmployee dev = developerEmployee("Daniel", 800, "PHP");
+    std::cout << "name: " << dev.getName() << std::endl;
+    std::cout << "salary: $" << dev.getSalary() << std::endl;
+    std::cout << "role: " << dev.getRole() << std::endl;
+    std::cout << "role description: " << dev.roleDescription() << std::endl;
+    std::cout << "main language: " << dev.getLanguage() << std::endl;
+
+    separator();
+
+    // seller entity
+    sellerEmployee seller = sellerEmployee("Jhonny", 825.50);
+    std::cout << "name: " << seller.getName() << std::endl;
+    std::cout << "salary: $" << seller.getSalary() << std::endl;
+    std::cout << "role: " << seller.getRole() << std::endl;
+    std::cout << "role description: " << seller.roleDescription() << std::endl;
+
     return 1;
 }
